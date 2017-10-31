cee9cc8f0dea75e19b18c00d2a71774dda05038c
= <=>
2017-10-31 20:46:19 +0000

    Add Templates.Template unit test file
    This file is incomplete as it needs the actual test code, to be added
    once the javaspace entry has been added

---
+++ JoeFoxTest/Templates/TemplateTest.java
@@ -0,0 +1,11 @@
+package JoeFoxTest.Templates;
+
+import JoeFox.Templates.Template;
+import junit.framework.TestCase;
+
+public class TemplateTest extends TestCase
+{
+    public void testTrue () {
+        assertTrue(true);
+    }
+}
