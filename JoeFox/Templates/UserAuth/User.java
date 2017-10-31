1631688abc512519a7f14d7cca1ecff1f55d22f7
= <=>
2017-10-31 14:14:10 +0000

    Add basic user account class template

---
+++ JoeFox/Templates/UserAuth/User.java
@@ -0,0 +1,43 @@
+package JoeFox.Templates.UserAuth;
+
+import JoeFox.Templates.Template;
+
+/**
+ * Class JoeFox.Templates.UserAuth.User
+ *
+ * A user class to be utilised by the system to hold user account credentials
+ * and any user account logic, such as password encryption.
+ *
+ * @author Joe Fox
+ * @version 1.0
+ * @since 2017-10-30
+ */
+
+public class User extends Template
+{
+    public String username;
+    public String passHash;
+
+    /**
+     * Noargs constructor required for javaspaces
+     */
+    public User () {
+    }
+
+    /**
+     * Constructor for initialising username and passHash
+     */
+    public User (String username, String insecurePass) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    public voic setUsername (String username) {
+        this.username = username;
+    }
+
+    public void setPassword (String password) {
+        //Stick password hashing code call here
+        //this.passHash = encrypted password
+    }
+}
