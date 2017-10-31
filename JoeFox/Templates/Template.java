c883d3974fa5c2c21e91796e11c80ee4c4d19575
Joe Fox <joe@Joes-MacBook-Pro.local>
2017-10-31 13:56:47 +0000

    Add basic template superclass

---
+++ JoeFox/Templates/Template.java
@@ -0,0 +1,18 @@
+package JoeFox.Templates;
+
+/**
+ * Class JoeFox.Templates.Template
+ *
+ * Implements a basic template superclass to be inherited by all templates
+ *
+ * @author Joe Fox joefoxdevelopment@gmail.com
+ * @version 1.0
+ * @since 2017-10-31
+ */
+
+public class Template //extends javaspace template
+{
+    public Template () {
+        //No args constructor required for javaspaces
+    }
+}
