fb6580fdcca6ca43d18db7d85f08998683e32c8b
= <=>
2017-10-31 14:42:17 +0000

    Add advert template class

---
+++ JoeFox/Templates/Adverts/Advert.java
@@ -0,0 +1,45 @@
+package JoeFox.Templates.Adverts;
+
+import JoeFox.Templates.Template;
+
+/**
+ * Class JoeFox.Templates.Adverts.Advert
+ *
+ * A class utilised by the system to represent adverts and the relevant data
+ *
+ * @author Joe Fox
+ * @version 1.0
+ * @since 2017-10-31
+ */
+
+public class Advert extends Template
+{
+    /**
+     * The title of the advert to be displayed
+     */
+    public String title;
+
+    /**
+     * A short description of the product being advertised
+     */
+    public String description;
+
+    /**
+     * The price of the advertised item
+     */
+    public Float price;
+
+    /**
+     * Noargs constructor required by javaspaces
+     */
+    public Advert () {}
+
+    /**
+     * Constructor for adverts to initialise title, description and price
+     */
+    public Advert (String title, String description, Float price) {
+        this.title       = title;
+        this.description = description;
+        this.price       = price;
+    }
+}
