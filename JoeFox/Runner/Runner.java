68978a967d421a57e62a98baec7b5092be6daeeb
= <=>
2017-11-09 13:35:42 +0000

    Remove System.out.println () call from Runner

--- JoeFox/Runner/Runner.java
+++ JoeFox/Runner/Runner.java
@@ -5,9 +5,7 @@ import JoeFox.Gui.*;
 public class Runner {
 
     public static void main (String args[]) {
-        System.out.println ("Starting");
         ContainerFactory factory = new ContainerFactory ();
         Container container      = factory.create();
     }
-
 }
